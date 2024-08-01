e1386073706ce309d5e35f1965a8635d50c587c6
Aresha Sohail <138809867+AreshaSohail1@users.noreply.github.com>
2024-08-01 05:35:13 -0700

    aresha

--- yalla/Components/moleculus/Clints.jsx
+++ yalla/Components/moleculus/Clints.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import Span from '../atoms/Span'
+import Container from '../atoms/Container'
 
 function Clints() {
   return (
     <div>
-        <div>
-            
-        </div>
+      <div className='bg-[#164b8d]  justify-between   px-[30px] h-[200px]  '>
+        <Container>
+          <div className='flex w-[50%]   py-[30px]'>
+            <div className=''>
+              <Span level='clintbox'> 1256</Span>
+              <div className=''>
+                <Span level='clint'>CLIENTS</Span>
+              </div>
+            </div>
+
+            <div>
+              <Span level='clintbox'> 1256</Span>
+              <div className=''>
+                <Span level='clint'>CLIENTS</Span>
+              </div>
+            </div>
+
+            <div>
+              <Span level='clintbox'> 1256</Span> 
+              <div className=''>
+                <Span level='clint'>CLIENTS</Span>
+              </div>
+            </div>
+            <div>
+              <Span level='clintbox'> 1256</Span>
+              <div className=''>
+                <Span level='clint'>CLIENTS</Span>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
