28f8268902741933019ace307b67eaa0cf67927f
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-08-02 05:50:04 -0700

    saqib

--- yalla/Components/moleculus/Header.jsx
+++ yalla/Components/moleculus/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
                         <li><AnchorTag variant='Headerh' href='/acservice'> AC Services</AnchorTag></li>
                         <li><AnchorTag variant='Headerh' href=''>over Servcies</AnchorTag></li>
                         <li><AnchorTag variant='Headerh' href='/tileservice'>Tiles Fixing</AnchorTag></li>
-                        <li><AnchorTag variant='Headerh' href=''>Electrician Services</AnchorTag></li>
+                        <li><AnchorTag variant='Headerh' href='/electricservice'>Electrician Services</AnchorTag></li>
                         <li><AnchorTag variant='Headerh' href=''>Painting & Partitions</AnchorTag></li>
                         <li><AnchorTag variant='Headerh' href=''>Cleaning Services</AnchorTag></li>
                         <li><AnchorTag variant='Headerh' href=''>Moving & Relocating</AnchorTag></li>
---
+++ yalla/Components/organisms/Electricservice.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Electricbanner from '../moleculus/Electricbanner'
+import Electricban1 from '../moleculus/Electricban1'
+import Electircban2 from '../moleculus/Electricban2'
+import ElectircHeading from '../moleculus/ElectricHeading'
+import Electricimages from '../moleculus/Electircimages'
+import Footer from './Footer'
+
+function Electricservice() {
+  return (
+    <div>
+         <Electricbanner/>
+         <Electricban1/> 
+         <Electircban2/>
+         <ElectircHeading/>
+         <Electricimages/>
+         <Footer/>
+      
+    </div>
+  )
+}
+
+export default Electricservice
