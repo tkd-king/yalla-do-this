ce5cfa5725e5b2c48052a8824403905faed8209d
Ayesha Mukhtar <144342578+AYESHA4356@users.noreply.github.com>
2024-08-02 18:51:12 +0500

    ayesha

--- yalla/Components/organisms/Banner.jsx
+++ yalla/Components/organisms/Banner.jsx
@@ -6,16 +6,16 @@ export default function Banner() {
   return (
     <div className=' '>
       <div className="baner-bg">
-        <div className="py-[15%]  ">
-          <div className='p-[10px] bg-[#45a6af] w-[20%] '>
+        <div className="py-[15%] px-[3%] grid gap-9 ">
+          <div className='py-[10px] text-white px-[20px] bg-[#45a6af] w-[23%] '>
             <Heading level="6">WELCOME TO YALLA DO THIS</Heading>
           </div>
-          <div className="">
+          <div className="grid gap-6 ">
             <Heading level="1">The temperature <br /> of cooling is in <br /> your hand.</Heading>
             <Heading level="4">Providing best-in-class air conditioning services <br /> to make your home cooler, more energy-efficient, <br /> and optimized for your comfort</Heading>
           </div>
-          <div className="bg-[#f35748] hover:bg-[#45a6af] w-[13%] p-[15px] ">
-            <button className='flex items-center text-white font-[600] '>Discover More  <MdArrowOutward /></button>
+          <div className="bg-[#f35748] hover:bg-[#45a6af] w-[17%]  p-[20px] ">
+            <button className='flex items-center text-white text-[18px]  font-[600] '>Discover More  <MdArrowOutward /></button>
           </div>
         </div>
       </div>
